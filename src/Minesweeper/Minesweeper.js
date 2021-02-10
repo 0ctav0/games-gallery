d5038bae737063038dcaa309f1c6bf0e1c21bde8
0ctav0 <fonlinetla2@mail.ru>
2021-02-10 19:12:35 +0500

    Minesweeper: basic functionality part 2

--- src/Minesweeper/Minesweeper.js
+++ src/Minesweeper/Minesweeper.js
@@ -6,14 +6,13 @@ function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-
-
 const EMPTY = 0;
 const MINE = 1;
 class CellModel {
 
   constructor(value) {
     this.value = value;
+    this.flag = false;
     this.opened = false;
     this.minesAroundNumber = "";
   }
@@ -26,12 +25,20 @@ class CellModel {
     return this.value === EMPTY;
   }
 
-  isOpened() {
-    return this.opened;
-  }
-
   getClasses() {
-    return "cell " + (this.isOpened() ? "opened " : "") + (this.hasMine() ? "mine " : "");
+    return "cell " 
+    + (this.opened ? "opened " : "")
+    + (this.opened && this.hasMine() ? "mine " : "")
+    + (!this.opened && this.flag ? "flag " : "")
+    ;
+  }
+}
+
+class SplashScreen extends React.Component {
+  render() {
+    return (
+      <div className="splash-screen" style={{...this.props.size, ...{display: this.props.show ? "" : "none"}}}/>
+    );
   }
 }
 
@@ -44,23 +51,36 @@ class Minesweeper extends React.Component {
     super(props);
 
     this.state = {
-      mineField : this.initMineField(props)
+      mineField : this.initMineField(props),
+      splashSize: {},
+      splashShow: false,
     };
+
+    this.fieldRef = React.createRef();
+    this.createCellRefs(props);
+  }
+
+  updateCell(cell, x, y) {
+    this.cellRefs[x][y].current.setState({cell});
   }
 
   initMineField(props) {
-    let mineField = [];
+    const mineField = [];
     for (let x = 0; x < props.col; x++) {
-      let column = new Array(props.row).fill(new CellModel(EMPTY));
+      const column = [];
+      for (let y = 0; y < props.row; y++) {
+        column.push(new CellModel(EMPTY));
+      }
       mineField.push(column);
     }
 
     let i = 0;
     while (i < props.minesCount) {
-      const randomX = randomInt(0, props.col - 1);
-      const randomY = randomInt(0, props.row - 1);
-      if (mineField[randomX][randomY].isEmpty()) {
-        mineField[randomX][randomY] = new CellModel(MINE);
+      const x = randomInt(0, props.col - 1);
+      const y = randomInt(0, props.row - 1);
+      const cell = mineField[x][y];
+      if (cell.isEmpty()) {
+        cell.value = MINE;
         i++;
       }
     }
@@ -68,59 +88,82 @@ class Minesweeper extends React.Component {
     return mineField;
   }
 
-  hasMine(x, y) {
-    return this.state.mineField[x][y].hasMine();
-  }
-
-  isOpened(x, y) {
-
-  }
-
-  goAround(originX, originY) {
-    const maxX = Math.min(originX+1, this.props.col-1);
-    const maxY = Math.min(originY+1, this.props.row-1);
-    let minesCount = 0;
-    // console.log("begin ", maxX, maxY, originX);
-    for (let x = Math.max(0,originX-1); x <= maxX; x++) {
-      for (let y = Math.max(0,originY-1); y <= maxY; y++) {
-        // console.log(x, " - ", y, " = ", this.state.mineField[x][y]);
-        if (x === originX && y === originY) continue;
-        const cell = this.state.mineField[x][y];
-        if (cell.hasMine()) {
-          minesCount++;
-        } else if (!cell.isOpened()) {
-          cell.opened = true;
-          this.goAround(x,y);
-        }
-        cell.minesAroundNumber = minesCount;
-        // console.log("minesCount", minesCount, this, this.props.c);
+  createCellRefs(props) {
+    this.cellRefs = [];
+    for (let x = 0; x < props.col; x++) {
+      const column = [];
+      for (let y = 0; y < props.row; y++) {
+        column.push(React.createRef());
       }
+      this.cellRefs.push(column);
     }
   }
 
-  openAround(cell, originX, originY) {
-    console.log("openAround");
-    console.log(this.props.children);
-    console.log(React.Children.count(this.props.children));
-    this.goAround(originX, originY);
+  clickHandler(originX, originY) {
+    const cell = this.state.mineField[originX][originY];
+    if (cell.hasMine()) {
+      cell.opened = true;
+      this.updateCell(cell, originX, originY);
+      this.setState({splashShow: true});
+    }
+    else if (!cell.flag) {
+      this.goAround(originX, originY);
+    }
+  }
 
+  rightClickHandler(event, x, y) {
+    event.preventDefault();
+    const cell = this.state.mineField[x][y];
+    cell.flag = true;
+    this.updateCell(cell, x, y);
+  }
 
-    // const minesAroundNumber = minesCount > 0 ? minesCount.toString() : "";
-    // cell.setState({minesAroundNumber});
+  goAround(originX, originY) {
+    let minesCount = 0;
+    const cellsNeedToCheck = [];
+    const maxX = Math.min(originX+1, this.props.col-1);
+    const maxY = Math.min(originY+1, this.props.row-1);
+    const originCell = this.state.mineField[originX][originY];
+    originCell.opened = true;
+    for (let x = Math.max(0,originX-1); x <= maxX; x++) {
+      for (let y = Math.max(0,originY-1); y <= maxY; y++) {
+        const cell = this.state.mineField[x][y];
+        if (cell.opened) {
+          continue;
+        }
+        cellsNeedToCheck.push([x,y]);
+        if (cell.hasMine()) {
+          minesCount++;
+        }
+      }
+    }
+    if (minesCount === 0) {
+      cellsNeedToCheck.map((xy) => this.goAround(xy[0],xy[1]));
+    }
+    originCell.minesAroundNumber = minesCount > 0 ? minesCount.toString() : "";
+    this.updateCell(originCell, originX, originY);
+  }
+
+  componentDidMount() {
+    this.setState({splashSize: {
+      width: this.fieldRef.current.clientWidth,
+      height: this.fieldRef.current.clientHeight
+    }});
   }
 
   render() {
     return (
-      <div className="minesweeper"><div className="field">
+      <div className="minesweeper"><div className="field" ref={this.fieldRef}>
+      <SplashScreen size={this.state.splashSize} show={this.state.splashShow}/>
       {[...Array(this.props.col).keys()].map((x) => {
         return <div key={x} className="col">
         {[...Array(this.props.row).keys()].map((y) => {
           return <Cell 
             key={[x,y]}
             cell={this.state.mineField[x][y]}
-            // classes={this.hasMine(x,y) ? "opened mine" : ""}
-            //minesAroundNumber={() => this.openAround(x,y)}
-            onClick={(cell) => this.openAround(cell,x,y)}
+            ref={this.cellRefs[x][y]}
+            onClick={() => this.clickHandler(x,y)}
+            onContextMenu={e => this.rightClickHandler(e,x,y)}
             />
         })}
         </div>
